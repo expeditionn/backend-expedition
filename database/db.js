22947eabcad334389137a1171723d10519b0222b
Yashi Gupta <yashi.g23csai@nst.rishihood.edu.in>
2024-09-29 21:15:42 +0530

    Connecting to online cluster

--- database/db.js
+++ database/db.js
@@ -9,7 +9,10 @@ const dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: true 
+    }
 };
 
 // Create a connection pool
